6707bd57eb52086965220b88475f87f9a6d3f6fe
Nader Frigui <nadfri@gmail.com>
2020-01-10 19:45:07 +0100

    Update buttons effects

--- script.js
+++ script.js
@@ -4,29 +4,12 @@ window.onload = function() {
 //------------------------Keyboard assigment-----------------------------
 //id not declared - not needed into onEvent
 
-class Keyboard{
+class Keyboard
+{
 	 constructor(element,key)
 	 {
 	 	this.element = element;
 	 	this.key= key;
-	 }
-
-	 effect()
-	 {
-	 	let bgColorIn  = "grey";
-	 	let bgColorOut = "#544f4f"; 
-
-	 	if(this.element == equal) {bgColorIn="#d37e2e"; bgColorOut="#5e5e5e"}
-
-
-	 	calculLine.onkeydown	  = (e)=>{ e.preventDefault();}; // no keypress
-
-	 	this.element.onmouseenter = () =>{ this.element.style.backgroundColor = bgColorIn;  };
-	 	this.element.onmouseout   = () =>{ this.element.style.backgroundColor = bgColorOut; };
-	 	
-	 	this.element.addEventListener("touchstart",() =>{ this.element.style.backgroundColor = bgColorIn;  });
-	 	this.element.addEventListener("touchend",  () =>{ this.element.style.backgroundColor = bgColorOut; });
-
 	 }
 
 
@@ -39,13 +22,9 @@ class Keyboard{
 		};
 
 	 }
- 
-}
 
-class SpecialKey extends Keyboard{ constructor(element,key)
-	{super(element,key);}
 
-	delete()
+	 delete()
 	{
 		this.element.onclick = () =>
 		{
@@ -53,88 +32,86 @@ class SpecialKey extends Keyboard{ constructor(element,key)
 			let start = calculLine.selectionStart;
 			let end   = calculLine.selectionEnd;
 
-			//switch (true) 
-			//{
-/*				
-				case (start-end == 0 && start != 0):
-					calculLine.value=text.slice(0,start-1) + text.slice(end); //del value before cursor
-					calculLine.selectionEnd = start-1; //keep focus in start-1 position
-					break;
+			if(start-end == 0 && start != 0)
+			{
+				calculLine.value = text.slice(0,start-1) + text.slice(end); //del value before cursor
+				calculLine.selectionEnd = start-1; //keep focus in start-1 position
+			}
 
-				default:
-					calculLine.value = text.slice(0,start) + text.slice(end); //del selection
-					calculLine.selectionEnd = start; //keep focus in start position
-					break;
-*/
-
-				if(start-end == 0 && start != 0)
-				{
-					calculLine.value=text.slice(0,start-1) + text.slice(end); //del value before cursor
-					calculLine.selectionEnd = start-1; //keep focus in start-1 position
-				}
-
-				else
-				{
-					calculLine.value = text.slice(0,start) + text.slice(end); //del selection
-					calculLine.selectionEnd = start; //keep focus in start position
-				}
-
-
-			//}
+			else
+			{
+				calculLine.value = text.slice(0,start) + text.slice(end); //del selection
+				calculLine.selectionEnd = start; //keep focus in start position
+			}
 
 			calculLine.focus();
 		};
 	}
+
+
+	clearScreen()
+	{
+		this.element.onclick = () => calculLine.value="";
+	}
+ 
 }
 
 
+const keyPlus       = new Keyboard(plus,"+"); 	    keyPlus.press();
+const keyMinus      = new Keyboard(minus,"-"); 	    keyMinus.press();
+const keyMultiplied = new Keyboard(multiplied,"x"); keyMultiplied.press();
+const keyDivided    = new Keyboard(divided,"/"); 	keyDivided.press();
+const keyPercent	= new Keyboard(percent,"%");	keyPercent.press();
+
+const keyParseL		= new Keyboard(parseL,"(");		keyParseL.press();
+const keyParseR		= new Keyboard(parseR,")");		keyParseR.press();
+const keySquare		= new Keyboard(square,"²");		keySquare.press();
+const keyRoot		= new Keyboard(root,"√");		keyRoot.press();
+const keyReverse	= new Keyboard(reverse,"⅟");		keyReverse.press();
+const keyPi			= new Keyboard(pi,"𝝅");			keyPi.press();
+const keyDot		= new Keyboard(dot,".");		keyDot.press();
+
+const keyZero	    = new Keyboard(zero,"0");		keyZero.press();
+const keyOne	    = new Keyboard(one,"1");		keyOne.press();
+const keyTwo	    = new Keyboard(two,"2");		keyTwo.press();
+const keyThree	    = new Keyboard(three,"3");		keyThree.press();
+const keyFour	    = new Keyboard(four,"4");		keyFour.press();
+const keyFive	    = new Keyboard(five,"5");		keyFive.press();
+const keySix	    = new Keyboard(six,"6");		keySix.press();
+const keySeven	    = new Keyboard(seven,"7");		keySeven.press();
+const keyEight	    = new Keyboard(eight,"8");		keyEight.press();
+const keyNine	    = new Keyboard(nine,"9");		keyNine.press();
+
+const keyDel	    = new Keyboard(del);			keyDel.delete();
+const keyClear		= new Keyboard(clear);			keyClear.clearScreen();
+const keyEqual	    = new Keyboard(equal,"=");		//keyEqual.press();
 
 
-const keyPlus       = new Keyboard(plus,"+"); 	    keyPlus.effect(); 		keyPlus.press();
-const keyMinus      = new Keyboard(minus,"-"); 	    keyMinus.effect(); 		keyMinus.press();
-const keyMultiplied = new Keyboard(multiplied,"x"); keyMultiplied.effect(); keyMultiplied.press();
-const keyDivided    = new Keyboard(divided,"/"); 	keyDivided.effect(); 	keyDivided.press();
-const keyPercent	= new Keyboard(percent,"%");	keyPercent.effect(); 	keyPercent.press();
 
-const keyParseL		= new Keyboard(parseL,"(");		keyParseL.effect(); 	keyParseL.press();
-const keyParseR		= new Keyboard(parseR,")");		keyParseR.effect(); 	keyParseR.press();
-const keySquare		= new Keyboard(square,"²");		keySquare.effect(); 	keySquare.press();
-const keyRoot		= new Keyboard(root,"√");		keyRoot.effect(); 		keyRoot.press();
-const keyReverse	= new Keyboard(reverse,"⅟");		keyReverse.effect(); 	keyReverse.press();
-const keyPi			= new Keyboard(pi,"𝝅");			keyPi.effect(); 		keyPi.press();
-const keyDot		= new Keyboard(dot,".");		keyDot.effect(); 		keyDot.press();
 
-const keyZero	    = new Keyboard(zero,"0");		keyZero.effect(); 		keyZero.press();
-const keyOne	    = new Keyboard(one,"1");		keyOne.effect(); 		keyOne.press();
-const keyTwo	    = new Keyboard(two,"2");		keyTwo.effect(); 		keyTwo.press();
-const keyThree	    = new Keyboard(three,"3");		keyThree.effect(); 		keyThree.press();
-const keyFour	    = new Keyboard(four,"4");		keyFour.effect(); 		keyFour.press();
-const keyFive	    = new Keyboard(five,"5");		keyFive.effect(); 		keyFive.press();
-const keySix	    = new Keyboard(six,"6");		keySix.effect(); 		keySix.press();
-const keySeven	    = new Keyboard(seven,"7");		keySeven.effect(); 		keySeven.press();
-const keyEight	    = new Keyboard(eight,"8");		keyEight.effect(); 		keyEight.press();
-const keyNine	    = new Keyboard(nine,"9");		keyNine.effect(); 		keyNine.press();
 
-const keyDel	    = new SpecialKey(del,"");		keyDel.effect();   		keyDel.delete();
-const keyEqual	    = new SpecialKey(equal,"=");	keyEqual.effect();
+function touchButton() //touch button effect on device replaces hover effect
+{
+	const td = document.getElementsByTagName("td");
 
-	
-calculLine.onmouseup = () => {console.log("start=" + calculLine.selectionStart + " end=" +calculLine.selectionEnd )};
+	for (let tds of td)
+		{
+			tds.addEventListener("touchstart",() =>{ 
+				if(tds == equal) equal.style.backgroundColor = "#d37e2e";
+				else tds.style.backgroundColor = "grey";  });
 
+			tds.addEventListener("touchend",() =>{ 
+				if(tds == equal) equal.style.backgroundColor = "#5e5e5e";
+				else tds.style.backgroundColor = "#544f4f"; });
+		}
+		
+}
+
+touchButton();
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
