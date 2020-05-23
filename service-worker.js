3ac881335cda2709c197284afff36b81b8c02682
Nader Frigui <nadfri@gmail.com>
2020-05-23 18:05:22 +0200

    modification of scope SW 2

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
   workbox.routing.registerRoute(
-    /\.(?:html|js|css|png|jpg|jpeg|ico|json)$/,
+    new RegExp('/*'), //cached all files
     new workbox.strategies.StaleWhileRevalidate()
   );
 } else {console.log(`Boo! Workbox didn't load 😬`);}
