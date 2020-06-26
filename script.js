bd0d2bc69a8da70985a7286e5db98679be7d74a5
Nader Frigui <nadfri@gmail.com>
2020-06-26 12:46:25 +0200

    add margin auto on page

--- script.js
+++ script.js
@@ -344,11 +344,9 @@ if('serviceWorker' in navigator)
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 window.onresize = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight*0.01}px`);
 
-
-}
+};
 
 
 
